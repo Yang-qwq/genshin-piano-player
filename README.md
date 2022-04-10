44d646db716824fbb62cfcc2219aeb702a218335
Yang_qwq <yang_qwq@qq.com>
2022-04-10 19:57:47 +0800

    合并提交历史

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-#player
+# genshin-piano-player
+## 介绍：
+这是一个基于`Python`以及`keyboard`模块的原神风物之琴/镜花只琴自动演奏器
+
+本程序理论仅能运行在`Windows`环境下，暂不考虑对其他系统的支持
+
+**注意：使用这个项目有可能会带来潜在的封禁的风险，请在游戏中使用时三思而后行！**
+
+## 开始使用
+```shell
+python3 -m pip install requirements.txt 
+python3 player.py <曲谱所在路径>
+```
+此外，请拷贝一份配置文件从`config.default.yml`到`config.yml`，否则程序会报错
+
+## 制谱规范
+可以参考以下文本，或者参考`songs/demo.txt`
+```text
+歌曲名称 字符串
+歌曲作者 字符串
+谱师 字符串
+1 # 每行之间演奏速度，以秒为单位
+附言（支持使用{newline}换行） 字符串
+---（本行不读）
+d1 d2 d3 # 在一行内的音符视为同时演奏的音符
+d1 # 末尾不要留空格，不然会有bug，也不要加注释，是方便讲解的
+d1
+d1
+# 空行视为休止符
+
+d3
+```
+
+## 配置文件讲解
+```yaml
+enable_driver: false # 是否启用驱动级控制
+enable_browser: true # 是否启用浏览器预览
+browser_live_url: 'https://yuxiangwang0525.github.io/Windsong-piano/' # 浏览器预览地址
+
+```
+
+## TODO
+[] midi转换器支持
+
+[] bpm支持
+
+[] 驱动级硬件模拟
+
+[] argparse支持
+
+[] 提升程序健壮性
+
+[] 自 助 选 歌 系 统
---
+++ player.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+# 本程序理论仅能运行在Windows环境下，暂不考虑对其他系统的支持
+import sys
+import time
+import keyboard
+import webbrowser
+import os
+import yaml
+import ctypes
+
+__windir__ = os.getenv('windir')
+__driver_path__ = __windir__ + '\\System32\\DiagSvcs\\DiagnosticsHub.StandardCollector.Proxy.dll'
+__action_status__ = False
+__pid__ = os.getpid()
+
+if not os.path.isfile(__driver_path__):
+    __driver_path__ = False
+else:
+    dd_dll = ctypes.windll.LoadLibrary(__driver_path__)
+
+dd_keys = {
+    'c': 503,
+    'n': 506,
+    'z': 501,
+    'd': 403,
+    # 'l': 409,
+    'w': 302,
+    'u': 307,
+    'e': 303,
+    'f': 404,
+    'y': 306,
+    'x': 502,
+    'g': 405,
+    'v': 504,
+    'r': 304,
+    # 'i': 308,
+    'a': 401,
+    'm': 507,
+    'h': 406,
+    'b': 505,
+    # 'k': 408,
+    'q': 301,
+    'j': 407,
+    # 'p': 310,
+    # 'o': 309,
+    't': 305,
+    's': 402,
+}
+
+__note2keys_data__ = {
+    # 1
+    'd1': 'z',
+    'r1': 'x',
+    'm1': 'c',
+    'f1': 'v',
+    's1': 'b',
+    'l1': 'n',
+    't1': 'm',
+
+    # 2
+    'd2': 'a',
+    'r2': 's',
+    'm2': 'd',
+    'f2': 'f',
+    's2': 'g',
+    'l2': 'h',
+    't2': 'j',
+
+    # 3
+    'd3': 'q',
+    'r3': 'w',
+    'm3': 'e',
+    'f3': 'r',
+    's3': 't',
+    'l3': 'y',
+    't3': 'u',
+
+    # 其他
+    ' ': 'pass',
+    '': 'pass'
+}
+
+
+class Song(object):
+    def __init__(self, lines: list):
+        # 处理乐谱
+        self.data = lines
+        self.song_name = lines[0]
+        self.author = lines[1]
+        self.mapper = lines[2]
+        self.speed = float(lines[3])
+        self.comment = lines[4].format(newline='\n')
+
+        # 读取后删除无用数据
+        # lines = []
+
+        for i in range(0, 6):
+            self.data.pop(0)
+
+    def print_meta(self):
+        print("""歌曲名称：{name}
+歌曲作者：{author}
+谱师：{mappper}
+附言：{comment}""".format(name=self.song_name, author=self.author, mappper=self.mapper, comment=self.comment))
+
+    def get_speed(self):
+        return self.speed
+
+    def get_map_data(self):
+        notes = []
+        for line in self.data:
+            notes.append(line.split(' '))
+        return notes
+
+    def note2keys(self):
+        # map_data = [[<notes>], [<notes>], [<notes>]]
+        map_data = self.get_map_data()
+        new_map_data = []
+        for note_lines in map_data:
+            # note_lines = [note_a, note_b, note_3]
+            new_list = []
+            for note in note_lines:
+                # note = 'd1'
+                new_key = __note2keys_data__[note]
+                new_list.append(new_key)
+            new_map_data.append(new_list)
+        return new_map_data
+
+
+def press(keys: list):
+    print(keys)
+    if __driver_path__ is not False and conf_enable_driver is True:
+        # 按下
+        for key in keys:
+            if key is not 'pass':
+                dd_dll.DD_key(dd_keys[key], 1)
+            else:
+                pass
+
+        # 抬起
+        for key in keys:
+            dd_dll.DD_key(dd_keys[key], 2)
+    else:
+        for key in keys:
+            if key is not 'pass':
+                keyboard.press(key)
+            else:
+                pass
+
+
+def f8exit():
+    print('f8 已被按下，停止演奏！')
+    print('正在杀死进程 pid={}'.format(__pid__))
+    os.kill(__pid__, 2)
+
+
+def main():
+    global conf_enable_driver
+    global __action_status__
+
+    # 读取配置文件
+    with open('config.yml', 'r', encoding='utf-8') as f:
+        conf = yaml.safe_load(f)
+    conf_enable_driver = conf['enable_driver']
+    conf_enable_browser = conf['enable_browser']
+    conf_browser_live = conf['browser_live_url']
+
+    if conf_enable_driver is True and __driver_path__ is False:
+        print('驱动不可用！请将config.yml中的enable_driver项设为false或者修复相关dll！')
+        sys.exit(10)
+
+    print('准备中，请稍后...')
+
+    # 读取歌曲
+    song_path = sys.argv[1]
+    with open(song_path, 'r', encoding='utf-8') as song:
+        song_lines = song.read().splitlines()
+
+    song = Song(song_lines)
+    song.print_meta()
+
+    if conf_enable_browser:
+        webbrowser.open(conf_browser_live)
+        time.sleep(3)
+
+    map_data = song.note2keys()
+    print('已准备好演奏歌曲，如果你已经切换到了指定的界面，请按 p 开始演奏，并且可以使用 F8 停止演奏')
+    keyboard.wait('p')
+
+    # 绑定退出热键
+    keyboard.add_hotkey('f8', f8exit)
+
+    # 开始
+    __action_status__ = True
+    for keys in map_data:
+        press(keys)
+        time.sleep(song.get_speed())
+        if __action_status__ is not True:
+            break
+
+    print('演奏完成！进程正在退出')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
